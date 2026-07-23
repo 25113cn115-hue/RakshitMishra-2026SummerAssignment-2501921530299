cc42a99114b72f160efd1ef9b014fd0e828bb925
25113cn115-hue <25113cn115@glbitm.ac.in>
2026-07-23 22:35:47 +0530

    Add files via upload

---
+++ ques5.c
@@ -0,0 +1,14 @@
+//WAP to find the sum of the digits of a number
+#include<stdio.h>
+int main()
+{
+    int n , sum=0;
+    printf("Enter the number\n");
+    scanf("%d", &n);
+    while(n!=0)
+    {   sum=sum+n%10;
+        n=n/10;}
+        
+        printf("The sum of the digits in your number is %d", sum);
+        return 0;
+}
